4d6d04c28a6acca98ebb908417aa9a9baf61c6b6
ChristelleDS <christelle.dasilva75@gmail.com>
2022-12-05 17:44:51 +0100

    add namespaces to url

--- lettings/urls.py
+++ lettings/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [
-    path('lettings/', views.index, name='lettings_index'),
-    path('lettings/<int:letting_id>/', views.letting, name='letting'),
+app_name = 'lettings'
 
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:letting_id>/', views.letting, name='letting'),
 ]
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
+app_name = 'oc_lettings_site'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    path("lettings/", include("lettings.urls", namespace="lettings")),
+    path("profiles/", include("profiles.urls", namespace="profiles")),
 ]
