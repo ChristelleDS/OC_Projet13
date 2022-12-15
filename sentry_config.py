35afbe6c9d5ace64381b9a2b6c5b39728583fff1
ChristelleDS <christelle.dasilva75@gmail.com>
2022-12-15 15:07:54 +0100

    upd sentry config

--- sentry_config.py
+++ sentry_config.py
@@ -1,8 +1,12 @@
 import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 
 sentry_sdk.init(
-    "https://<key>@sentry.io/<project>",
-
+    dsn="https://6f6dd5b2d306491c9d3818fd52dcf6f1@o4504333296205824.ingest.sentry.io/4504333307215872",
+    integrations=[
+        DjangoIntegration(),
+    ],
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
